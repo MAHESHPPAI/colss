97ebd4a24358272c143cdd620b5ea2ff725dcb83
Siva <sivaprasada2004@gmail.com>
2026-02-21 15:33:44 +0530

    create sigma funcs and ensuref proper working

--- binding.cpp
+++ binding.cpp
@@ -1,11 +1,6 @@
-#include <pybind11/pybind11.h>
 #include "fun/sum.hpp"
+#include <pybind11/pybind11.h>
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(colss, m) {
-    py::class_<Sum>(m, "Sum")
-        .def(py::init<>())
-        .def("sum_double", &Sum::sum<double>)
-        .def("sum_i64", &Sum::sum<std::int64_t>);
-}
+PYBIND11_MODULE(colss, m) { m.def("sigma", &sigma, py::arg("expr")); }
--- fun/sum.hpp
+++ fun/sum.hpp
@@ -1,23 +1,68 @@
 #pragma once
 
-#include <pybind11/numpy.h>
-#include <cstdint>
+#include "../include/exprtk.hpp"
 #include <omp.h>
+#include <pybind11/numpy.h>
+#include <pybind11/pytypes.h>
 
+using namespace std;
 namespace py = pybind11;
 
-class Sum {
-public:
-    template<typename T>
-    T sum(py::array_t<T> arr) {
-        auto buf = arr.template unchecked<2>();
-        T share = 0;
+double sigma(string expr, py::args headers_and_arr) {
+    using T = double;
 
-        #pragma omp parallel for reduction(+:share)
-        for (ssize_t i = 0; i < buf.shape(0); i++) {
-            share += buf(i, 0);
-        }
+    size_t n_vars = headers_and_arr.size();
 
-        return share;
+    vector<string> names;
+    vector<py::detail::unchecked_reference<T, 1>> buffers;
+
+    names.reserve(n_vars);
+    buffers.reserve(n_vars);
+
+    for (auto arg : headers_and_arr) {
+        auto t = arg.cast<py::tuple>();
+
+        string name = t[0].cast<string>();
+        py::array_t<T> arr = t[1].cast<py::array_t<T>>();
+
+        names.push_back(name);
+        buffers.push_back(arr.unchecked<1>());
     }
-};
+
+    size_t n = buffers[0].shape(0);
+
+    for (size_t i = 1; i < buffers.size(); ++i)
+        if (buffers[i].shape(0) != n)
+            throw runtime_error("Array size mismatch");
+
+    double share = 0.0;
+
+#pragma omp parallel reduction(+ : share)
+    {
+        // 🔥 Thread-local engine
+        exprtk::symbol_table<T> symbol_table;
+        exprtk::expression<T> expression;
+        exprtk::parser<T> parser;
+
+        vector<T> variables(n_vars, 0.0);
+
+        for (size_t i = 0; i < n_vars; ++i)
+            symbol_table.add_variable(names[i], variables[i]);
+
+        symbol_table.add_constants();
+        expression.register_symbol_table(symbol_table);
+
+        if (!parser.compile(expr, expression))
+            throw runtime_error("Expression compile failed");
+
+#pragma omp for
+        for (size_t i = 0; i < n; ++i) {
+            for (size_t j = 0; j < n_vars; ++j)
+                variables[j] = buffers[j](i);
+
+            share += expression.value();
+        }
+    }
+
+    return share;
+}
